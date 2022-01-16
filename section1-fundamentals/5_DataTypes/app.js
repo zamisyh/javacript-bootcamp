7efc436890a2453998ddf7e8cfd7b6c6e21b47b2
zam-dev <zamsyh.work@gmail.com>
2022-01-16 13:05:53 +0700

    Bootcamp section 1 - Data Types

---
+++ section1-fundamentals/5_DataTypes/app.js
@@ -0,0 +1,32 @@
+//primitive 
+let item
+//undefined
+
+//String
+item = 'Hallo'
+
+//Number
+item = 123
+
+//Bolean
+item = true
+
+//Null
+item = null
+
+//Symbol
+item = Symbol()
+
+//Reference type
+
+//array
+item = [1, 2, 3] 
+//object
+item = {
+    name: "Zamzam"
+}
+
+//Date
+item = new Date()
+console.log('type', typeof item)
+
