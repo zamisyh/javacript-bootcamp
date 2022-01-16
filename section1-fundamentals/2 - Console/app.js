db9e29941b9bb0efe2995ec1b7774fdf066f56f7
zam-dev <zamsyh.work@gmail.com>
2022-01-16 12:39:00 +0700

    Bootcamp section 1

---
+++ section1-fundamentals/2 - Console/app.js
@@ -0,0 +1,36 @@
+console.log("Hallo Worldes!"); // tile
+console.log(123) // integer
+console.log(true) // boolean
+
+//variable
+var message = "This is a new message";
+console.log(message);
+//array
+console.log([1,2,3]);
+//object
+console.log({
+    name: 'Zamzam',
+    age: 18
+});
+//table
+console.table({
+    name: 'Zamzam',
+    age: 18
+});
+//error or warn
+console.error("Error message!");
+console.warn("Error warn");
+
+//clear the console
+console.clear();
+
+//time
+console.time('time');
+console.log('timing');
+console.log('timing');
+console.log('timing');
+console.log('timing');
+console.log('timing');
+console.log('timing');
+console.log('timing');
+console.timeEnd('time');
