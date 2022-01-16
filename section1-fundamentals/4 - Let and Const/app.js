c599def06b8596f8311cf344b008c8b3dfe3c173
zam-dev <zamsyh.work@gmail.com>
2022-01-16 12:50:32 +0700

    Bootcamp section 1 - let and const

---
+++ section1-fundamentals/4 - Let and Const/app.js
@@ -0,0 +1,20 @@
+// let name = "Alan"
+// console.log(name)
+// name = "John"
+// console.log(name)
+
+// const name = "Alan"
+// name = "John"  //error with const but not error using let
+
+// const user = {
+//     name: 'Alan',
+//     age: 30
+// }
+// console.log(user)
+// user.name = "John"
+// console.log(user)
+
+// const arr = []
+// console.log(arr)
+// arr.push([3, 4])
+// console.log(arr)
