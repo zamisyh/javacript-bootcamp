bb833a40dcfdbcb7256cc720f9bbf032b6f084f6
zam-dev <zamsyh.work@gmail.com>
2022-01-16 13:17:06 +0700

    Bootcamp section 1 - Reference VS Primitive

---
+++ section1-fundamentals/6_PrimitiveVSReference/app.js
@@ -0,0 +1,39 @@
+// let x = 20
+// let y = x
+
+// console.log(x, y) // 20, 20
+// x = 30
+// console.log(x, y) // 30, 20
+
+// let a = { value: 10 }
+// let b = a
+// console.log(a, b) // 10, 10
+// a.value = 30
+// console.log(a, b) // 30, 30
+
+// let name = "Alan"
+// function message(name){
+//     name = "Peter"
+//     console.log("Name inside function : ", name)
+// }
+// message(name)
+// console.log(name)
+
+// Name inside function :  Peter
+// Alan
+
+
+//Reference
+// let user = {
+//     name: "Alan"
+// }
+
+// function message(user){
+//     user.name = "Peter"
+//     console.log("User inside function : " , user.name)
+// }
+// message(user)
+// console.log(user.name)
+
+// Name inside function :  Peter
+// Peter
