1d1c1a1d2cb25fa0dd5abe2fb9cf82c1a1fc7b35
zam-dev <zamsyh.work@gmail.com>
2022-01-17 19:37:28 +0700

    Bootcamp section 1 - Other String Method

---
+++ section1-fundamentals/11_OtherStringMethods/app.js
@@ -0,0 +1,29 @@
+const firstName = 'Zamzam'
+const lastName = 'Saputra'
+
+let val;
+
+//concatenate
+val = firstName + lastName // ZamzamSaputra
+val = firstName + ' ' + lastName // Zamzam Saputra
+
+//Append
+val = 'Zamzam'
+val += 'Saputra'
+// ** result = ZamzamSaputra
+
+//Escaping
+val = "That's great. that you've very found a job"
+val = 'That\'s great. that you\'ve very found a job'
+
+//length
+val = firstName.length //6
+
+//concat
+val = firstName.concat(' ', lastName) //Zamzam Saputra
+
+//change case
+val = firstName.toUpperCase() //ZAMZAM
+val = firstName.toLowerCase() //zamzam
+
+console.log(val)
