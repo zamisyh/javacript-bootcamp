57d260433a47dde87ff13a7e346f5f4431c8edaf
zam-dev <zamsyh.work@gmail.com>
2022-02-09 20:12:03 +0700

    Bootcamp section 1 - If Statements

---
+++ section1-fundamentals/17_IfStatements/app.js
@@ -0,0 +1,37 @@
+const mark = '50'
+
+//Abstract equality comparison
+// if (mark == 50) {
+//     console.log('The mark is 50')
+// }else{
+//     console.log('The mark is NOT 50')
+// }
+
+// if (mark != 40) {
+//     console.log('The mark NOT 40')
+// }else{
+//     console.log('The mark is 40')
+// }
+
+//Strictly equality comparison
+//check type variable if string, is not 50
+// if (mark === 50) {
+//     console.log('The mark is 50')
+// }else{
+//     console.log('The mark is NOT 50')
+// }
+
+
+//check undefined
+// if (typeof mark !== 'undefined') {
+//     console.log(`The mark is ${mark}`)
+// }else{
+//     console.log('The mark is undefined')
+// }
+
+//greater than or equal
+if (mark >= 50) {
+    console.log('Yoou pass the test')
+} else {
+    console.log('You fail the test')
+}
