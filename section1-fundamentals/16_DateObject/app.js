851bde3d02abb61f7161582245ac087b32045d45
zam-dev <zamsyh.work@gmail.com>
2022-02-09 20:02:07 +0700

    Bootcamp section 1 - Date Object

---
+++ section1-fundamentals/16_DateObject/app.js
@@ -0,0 +1,19 @@
+let val;
+
+const date = new Date()
+
+val = date.getMonth()
+val = date.getDate()
+val = date.getDay()
+val = date.getFullYear()
+val = date.getHours()
+val = date.getMinutes()
+val = date.getSeconds()
+val = date.getTime()
+
+// date.setMonth(0)
+// date.setDate(31)
+// date.setFullYear(2003)
+
+console.log(date);
+console.log(val);
