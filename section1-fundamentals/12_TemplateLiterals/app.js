959e27624f6dad16a7ed7a13aca37e7c9aa9bd7b
zam-dev <zamsyh.work@gmail.com>
2022-02-09 14:41:33 +0700

    Bootcamp section 1 - Template Literals

---
+++ section1-fundamentals/12_TemplateLiterals/app.js
@@ -0,0 +1,18 @@
+let name = "Zamzam"
+let age = 19
+let dringking = 'Tea'
+let message;
+
+function drink(){
+    if (age > 18) {
+        return 'beer'
+    } else {
+        return 'tea'
+    }
+}
+
+message = 'Hallo, my name is ' + name + ', I love drinking ' + dringking;
+message = `Hallo, my name is ${name}, I love dringking ${drink()}` // template literals
+message = 'Hallo, my name is ' + name + ', I love dringking ' 
+            + (age > 18 ? 'beer' : 'tea')
+console.log(message)
