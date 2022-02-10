5172ee791712f08b0ec2b5fd539cc2c2e2bd732a
zam-dev <zamsyh.work@gmail.com>
2022-02-10 21:07:25 +0700

    Bootcamp section 1 - Logical Operator

---
+++ section1-fundamentals/18_LogicalOperator/app.js
@@ -0,0 +1,35 @@
+// logical operator
+// let mark = 56
+
+// // and operator
+// if (mark > 50 && mark <= 80) {
+//     console.log("Good")
+// }else if(mark > 80){
+//     console.log('Excellent !!')
+// }else{
+//     console.log('Keep it up')
+// }
+
+// or operator
+// let hour = 12;
+// let isWeekend = true;
+
+// if (hour < 10 || hour > 17 || isWeekend) {
+//     console.log('The office is now closed')
+// }else{
+//     console.log("The office is now opened")
+// }
+
+//ternary operator
+
+// let age = 18
+// const drink = age < 18 ? 'You cannot drink beer'
+//             : 'You can drink beer'
+// console.log(drink)
+
+
+// one line
+let age = 18
+
+if (age < 18) console.log("You cannot drink beer")
+else console.log("You can drink beer")
