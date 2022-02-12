ae44f8df9060ce1d7e6715cf53db22be245f51fd
zam-dev <zamsyh.work@gmail.com>
2022-02-12 13:21:42 +0700

    Bootcamp section 1 - General Loops

---
+++ section1-fundamentals/22_GeneralLoop/app.js
@@ -0,0 +1,29 @@
+//example for loop
+
+// for (let i = 0; i < 10; i++) {
+//    if (i === 3) {
+//       console.log('Three');
+//       continue; 
+//    }
+
+//    if (i === 6) {
+//       console.log(i)
+//       break;
+//    }
+      
+//    console.log(i)
+// }
+
+//example while loop
+// let i = 0;
+// while (i < 10) {
+//     console.log(i)
+//     i++;
+// }
+
+//examole do while
+// let i = 0;
+// do{
+//     console.log(i)
+//     i++;
+// } while(i < 10)
