61f0f8c9b9b012ba3dd2d8f4b8637273102e85ad
zam-dev <zamsyh.work@gmail.com>
2022-02-12 13:43:00 +0700

    Bootcamp section 1 - Window Object Methods

---
+++ section1-fundamentals/24_WindowObjectMethods/app.js
@@ -0,0 +1,13 @@
+//alert
+//alert('Hallo, Zamzam!)
+
+//prompt
+// const name = prompt('What is your name ?')
+// alert(`Hallo, ${name}`)
+
+//confirm
+if (confirm('Are you sure to delete ?')) {
+    alert('The file is deleted!')   
+}else{
+    alert('Nothing is deleted!')
+}
