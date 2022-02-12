81ffad2b7f29c7e605c4757542e52b8240a31a81
zam-dev <zamsyh.work@gmail.com>
2022-02-12 13:57:06 +0700

    Bootcamp section 1 - Window Object Properties

---
+++ section1-fundamentals/25_WindowObjectProperties/app.js
@@ -0,0 +1,38 @@
+//outer width and height
+// let oh = window.outerHeight;
+// let ow = window.outerWidth;
+// console.log(oh, ow)
+
+//inner width and height
+// let ih = window.innerHeight;
+// let iw = window.innerWidth;
+// console.log(ih, iw)
+
+//scroll points
+// let sy = window.scrollY;
+// let sx = window.scrollX;
+// console.log(sx, sy)
+
+//Location object
+// let val;
+// val = window.location.hostname;
+// val = window.location.port;
+// val = window.location.href;
+// val = window.location.search;
+// console.log(val)
+
+//redirect
+// window.location.href = 'http://www.google.com';
+
+//reload
+// window.location.reload()
+
+//history
+// window.history.go(-1)
+
+//navigator object
+let val;
+val = window.navigator;
+
+
+console.log(val.platform)
