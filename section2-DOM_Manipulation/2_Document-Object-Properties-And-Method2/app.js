ca6d70362e33dfb2cf1242324ff85777ea1ea2f8
zam-dev <zamsyh.work@gmail.com>
2022-02-13 21:40:42 +0700

    Bootcamp Section 2 - Document Object Properties And METHOD 2

---
+++ section2-DOM_Manipulation/2_Document-Object-Properties-And-Method2/app.js
@@ -0,0 +1,13 @@
+let doc;
+doc = document
+doc = document.all
+doc = document.all(1)
+doc = document.head
+doc = document.body
+doc = document.domain
+doc = document.URL
+doc = document.characterSet
+doc = document.contentType
+doc = document.links
+doc = document.links[0].classList
+console.log(doc)
