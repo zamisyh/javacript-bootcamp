c749ec54b408f092bbb934a840b27578fb6b1637
zam-dev <zamsyh.work@gmail.com>
2022-02-13 21:51:45 +0700

    Bootcamp Section 2 - Get Element By Id

---
+++ section2-DOM_Manipulation/3_Get-Element-By-Id/app.js
@@ -0,0 +1,10 @@
+let title;
+title = document.getElementById('name-title')
+title = document.getElementById('name-title').id
+title = document.getElementById('name-title').style.background = 'red'
+title = document.getElementById('name-title').style.color = 'blue'
+title = document.getElementById('name-title').style.padding = '15px'
+title = document.getElementById('name-title').textContent = "New Title"
+title = document.getElementById('name-title').innerText = 'Another title'
+
+console.log(title)
