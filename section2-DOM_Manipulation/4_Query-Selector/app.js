1f641439b6f07afbe90875c0443bd6b3e2d9e12b
zam-dev <zamsyh.work@gmail.com>
2022-02-14 22:06:30 +0700

    Bootcamp Section 2 - Query Selector

---
+++ section2-DOM_Manipulation/4_Query-Selector/app.js
@@ -0,0 +1,14 @@
+let item;
+item = document.querySelector('#name-title')
+item = document.querySelector('.collection-item')
+item = document.querySelector('#name-title')
+item = document.querySelector('h4')
+item = document.querySelector('li')
+item = document.querySelector('.collection-item').style.background = 'green'
+item = document.querySelector('.collection-item:last-child').style.background = 'red'
+item = document.querySelector('.collection-item:nth-child(3)').style.background = 'yellow'
+item = document.querySelector('.collection-item:nth-child(2)').style.background = 'blue'
+item = document.querySelector('.collection-item:nth-child(odd)').textContent = 'Zamzam'
+
+
+console.log(item)
