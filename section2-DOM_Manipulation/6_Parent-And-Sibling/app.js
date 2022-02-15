2054d6c5f14a1812afbb9c3f8dec2d17c3940774
zam-dev <zamsyh.work@gmail.com>
2022-02-15 21:13:35 +0700

    Bootcamp Section 2 - Parent and Siblings

---
+++ section2-DOM_Manipulation/6_Parent-And-Sibling/app.js
@@ -0,0 +1,16 @@
+const ul = document.querySelector('ul.collection');
+let item = ul.firstChild;
+item = ul.firstElementChild;
+item = ul.lastChild;
+item = ul.lastElementChild;
+item = ul.childElementCount;
+item = ul.firstElementChild;
+let li = ul.firstElementChild;
+item = li.parentElement;
+item = li.parentNode;
+item = li.nextElementSibling;
+item = li.nextSibling;
+item = li.nextElementSibling.nextElementSibling;
+item = li.nextElementSibling.previousSibling;
+
+console.log(item)
