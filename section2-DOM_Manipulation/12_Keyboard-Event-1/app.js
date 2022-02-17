31347a91606669acf0fe5f985495a915d9069f64
zam-dev <zamsyh.work@gmail.com>
2022-02-17 19:47:53 +0700

    Bootcamp Section 2 - Keyboard Event: Keypress, keyup, keydown

---
+++ section2-DOM_Manipulation/12_Keyboard-Event-1/app.js
@@ -0,0 +1,7 @@
+const firstNameInput = document.getElementById('first_name');
+// firstNameInput.addEventListener('keydown', run);
+// firstNameInput.addEventListener('keyup', run);
+firstNameInput.addEventListener('keypress', run);
+function run(e){
+    console.log('event type: ' + e.type + e.keyCode)
+}
