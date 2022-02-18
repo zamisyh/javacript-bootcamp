97285e0edafb6446f075b5610cd60eaf59d788f5
zam-dev <zamsyh.work@gmail.com>
2022-02-18 20:06:31 +0700

    Bootcamp Section 2 - Event : Onchange, submit

--- section2-DOM_Manipulation/13_Keyboard-Event-2/app.js
+++ section2-DOM_Manipulation/13_Keyboard-Event-2/app.js
@@ -1,15 +1,13 @@
 const firstNameInput = document.getElementById('first_name');
 const heading = document.querySelector('h3');
-
-// firstNameInput.addEventListener('focus', run);
-// firstNameInput.addEventListener('blur', run);
-// firstNameInput.addEventListener('cut', run); // ctrl + x
-// firstNameInput.addEventListener('paste', run); // ctrl + v
-// firstNameInput.addEventListener('copy', run);
-firstNameInput.addEventListener('input', run);
+const form = document.querySelector('form');
+const select = document.querySelector('select');
+form.addEventListener('submit', run)
+select.addEventListener('change', run)
 
 
 function run(e){
+    e.preventDefault()
     console.log('event type: ' + e.type)
     heading.innerText = e.target.value;
 }
