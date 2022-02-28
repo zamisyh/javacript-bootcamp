f2dc79c1db911b3c8b4a01ad15a4333461564030
zam-dev <zamsyh.work@gmail.com>
2022-02-28 13:34:02 +0700

    Bootcamp Section 4 - Arrow Function

---
+++ section4-XHR_Ajax_FetchAPI/14_Arrow_Function/app.js
@@ -0,0 +1,24 @@
+// const greeting = () => {
+//      console.log('Greeting')
+// }
+
+// const greeting = () => 
+// console.log('Greeting')
+
+// const greeting = () => 'Greeting'
+// console.log(greeting())
+
+// const greeting = function () {
+//      return 'Greeting'
+// }
+
+// const greeting = () => ({
+//      greeting: 'Hello'
+// });
+// console.log(greeting());
+
+const greeting = (firstName, lastName) => {
+     console.log(`Hallo, my name is ${firstName} ${lastName}`)
+}
+greeting('Zamzam', 'Saputra');
+
